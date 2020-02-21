86ca1a7d1f9729a13c451fdf12df89b9ec68b472
JC <zxcjhaycee@gmail.com>
2020-02-21 21:42:33 +0800

    Initial Commit

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/material/material.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/css/material/material-dashboard-pro.min.css">
+    <link rel="stylesheet" href="/css/datepicker.css">
+    <link rel="stylesheet" href="/css/form.css">
+    <title>Ordinario Pawnshop</title>
+</head>
+<body>
+    
+</body>
+<script src="/js/material/core/jquery-3.3.1.js"></script>
+<script src="/js/material/core/popper.min.js"></script>
+<script src="/js/material/plugins/jasny-bootstrap.min.js"></script>
+<script src="/js/material/core/bootstrap-material-design.min.js"></script>
+<script src="/js/material/plugins/arrive.min.js"></script>
+<script src="/js/material/plugins/bootstrap-selectpicker.js"></script>
+<script src="/js/material/plugins/moment.min.js"></script>
+<script src="/js/material/plugins/perfect-scrollbar.jquery.min.js"></script>
+<script src="/js/material/material-dashboard.js"></script>
+<script src="/js/datepicker.js"></script>
+<script src="/js/datepicker.en.js"></script>
+<script src="/js/form.js"></script>
+<script>
+    $('.air_date_picker').datepicker({
+    language: 'en',
+    todayButton: new Date(),
+    autoClose : true
+    // inline : true,
+    // minDate: new Date() // Now can select only dates, which goes after today
+});
+</script>
+</html>
